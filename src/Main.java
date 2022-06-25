bbec99d9c171f93ac395f5e22f6f8931dda60505
LightCode5794 <trantronghoang5794@gmail.com>
2022-06-25 21:49:00 +0700

    update icon game

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,16 @@
 import Controller.Home;
 
-public class Main {
+import javax.swing.*;
+import java.util.Objects;
+
+public class Main  {
+    ImageIcon imageIcon = new ImageIcon(Objects.requireNonNull(getClass().getResource("Icons/bomb.png")));
     public static void main(String[] args) {
 
+            Main main = new Main();
             Home homePanel = new Home();
+            homePanel.setIconImage(main.imageIcon.getImage());
+            homePanel.setTitle("Minesweeper");
             homePanel.setVisible(true);
 
     }
